02d71fddf0a6bc7d6397d4a8d07d0c48262ee9d0
AjanthSutharsan <asutharsan.63475@farnborough.ac.uk>
2017-11-29 16:09:26 +0000

    Add files via upload

---
+++ src/Main.clj
@@ -0,0 +1,39 @@
+(ns .polyglots)
+
+
+(def list1 (list 1 2 3))
+(def list2 '(1 2 3))
+(println list1)
+(println list2)
+
+(def vector1 (vector 1 2 3))
+(def vector2 [1 2 3])
+(println vector1)
+(println vector2)
+
+(def map1 (hash-map 1 10 2 20 3 30))
+(def map2 {1 10 2 20 3 30})
+(println map1)
+(println map2)
+
+(def set1 (hash-set 1 2 3))
+(def set2 #{1 2 3})
+(println set1)
+(println set2)
+
+(println (float 1))
+
+(print "Hello:")
+(println (count (list 1 2 3)))
+
+(def x (list 1 2 3 4 5))
+
+(println (every? even? x))
+(println (some? x))
+(println (some even? x))
+
+(println (take 100 (cycle '(1 2 3))))
+
+(println (conj '(1 2 3) 4))
+(println (into '(1 2 3) '(4 5 6)))
+(println (concat '(1 2 3) '(4 5 6)))
